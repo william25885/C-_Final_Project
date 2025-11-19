f413c42cfc855a3eefcf090397f45baf36ad17bd
william25885 <william25885@gmail.com>
2025-11-19 14:14:08 +0800

    Create message.cpp

---
+++ project_group32/message.cpp
@@ -0,0 +1,51 @@
+#include"message.h"
+#include<iostream>
+
+void message1()
+{
+    std::cout << "In an era of warlords, in a fantastical realm devoid of a fiend, people still strive for their individual ideals, forging alliances, forming factions, and harboring enmity towards specific individuals or regions." << std::endl;
+    return;
+}
+
+void message2()
+{
+    std::cout << "Amidst this tumultuous world, it's a minuscule tale, yet it narrates the joys and sorrows, the comings and goings, of a group of people." << std::endl;
+    return;
+}
+
+void message3()
+{
+    std::cout << "They vie not for riches or glory but simply for resources enough to sustain their existence. It's a struggle for survival in a world that demands their utmost efforts merely to endure." << std::endl;
+    return;
+}
+
+void message4()
+{
+    std::cout << "(wizard 1):(to wizard 2)I mourn the necessity for my brother and me to compete for the right to survive. Yet, ever since we chose diverging paths, we've sealed the fate of our current circumstances." << std::endl;
+    std::cout << "(wizard 2): I don't see it that way. From childhood to now, you've always drawn everyone's attention, praised for all you do. I've merely been a backdrop, compared to you. I'm tired of this life. For whatever reason, I want to give my all to bring you down." << std::endl;
+    return;
+}
+
+void message5()
+{
+    std::cout << "(tank 1):Now's not the time for you twin brothers to feud. Sharpen your blades; this is a battlefield, not a petty game of play house." << std::endl;
+    return;
+}
+
+void message6()
+{
+    std::cout << "(archer 2):Let's partake in this bloody party of sorts, and offer our all to each other!" << std::endl;
+    return;
+}
+
+void message7()
+{
+    std::cout << "(knight 1&2):Exactly to my liking." << std::endl;
+    return;
+}
+
+void message8()
+{
+    std::cout << "\"Dust to dust, soil to soil. In this battle, someone emerges victorious, yet it doesn't ensure smooth days ahead. Resources remain scarce; we're still in an era of warlords. The current mission remains giving our all to survive in this world.\"" << std::endl;
+    return;
+}
